92b3b8effeab13694983dfa2a1f1f2db0405d3ac
lutfiandri <lutfiandri232@gmail.com>
2021-04-28 15:14:15 +0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Regression;
+
+namespace RegressionCalculator
+{
+    static class Program
+    {
+        /// <summary>
+        ///  Aplikasi untuk menghitung garis regresi dengan beberapa metode regresi dari mata kuliah Metode Numeris.
+        ///  Saat ini masih console, rencana ke depan ingin diubah ke dalam WebAPI / Windows Form.
+        /// </summary>
+        //[STAThread]
+        static void Main()
+        {
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+
+        }
+    }
+}
